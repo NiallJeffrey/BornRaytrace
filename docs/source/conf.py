d5646d8d4ae60ed4c51a35d54db039663215f258
Niall Jeffrey <niall.jeffrey.15@ucl.ac.uk>
2021-11-12 15:15:39 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,16 +28,11 @@ author = 'Niall Jeffrey'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones..
+
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+    'sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary',
+    'sphinx.ext.mathjax'
 ]
-
-# napoleon_google_docstring = False
-# napoleon_use_param = False
-# napoleon_use_ivar = True
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
